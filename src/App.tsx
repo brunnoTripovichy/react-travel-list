5edb69503f35400d7121c26ad3c7f61449de9cbe
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-12 12:40:07 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Test
+      </button>
+    </div>
+  );
+};
+
+export default App;
